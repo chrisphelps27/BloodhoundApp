c5089d82ba381807900eb6abcf4223e86ae8ca65
grant47710 <troygrant8150@gmail.com>
2017-10-25 12:08:28 -0400

    Troys Test

--- app/src/main/java/com/mvctc/gw/bloodhoundapp/MainActivity.java
+++ app/src/main/java/com/mvctc/gw/bloodhoundapp/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    //Steve Yay
-    //Chris's Comment to test stuff
+    /*
+    Steve Yay
+    Chris's Comment to test stuff
+    Troy's boredom
+    */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
