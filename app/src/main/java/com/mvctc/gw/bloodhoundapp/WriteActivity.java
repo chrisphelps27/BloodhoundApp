c2cab6aceea39c7f95489184d4b17da0ab42ac5f
phelps47387 <chrisphelps2727@gmail.com>
2017-10-26 14:11:41 -0400

    Adds new activity files for acting as a location, sending ID information, or writing to an ID tag

---
+++ app/src/main/java/com/mvctc/gw/bloodhoundapp/WriteActivity.java
@@ -0,0 +1,19 @@
+package com.mvctc.gw.bloodhoundapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by phelps47387 on 10/26/2017.
+ */
+
+public class WriteActivity {
+    public class SendActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            //TODO change this to the correct activity once created
+            setContentView(R.layout.activity_main);
+        }
+    }
+}
