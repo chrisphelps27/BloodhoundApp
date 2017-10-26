2fb94d1e5be3da08e26f7ea71c16efea485827a2
phelps47387 <chrisphelps2727@gmail.com>
2017-10-26 14:12:20 -0400

    Adds buttons and onClick for buttons

--- app/src/main/java/com/mvctc/gw/bloodhoundapp/MainActivity.java
+++ app/src/main/java/com/mvctc/gw/bloodhoundapp/MainActivity.java
@@ -1,18 +1,52 @@
 package com.mvctc.gw.bloodhoundapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+/*
+    Authors: Chris Phelps, Steve McIntosh, Troy Grant
+*/
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
-    /*
-    Steve Yay
-    Chris's Comment to test stuff
-    Troy's boredom
-    */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TODO have it default to whichever activity they select after first-time run which will be saved in a config file, excluding writing to a tag
+
+        //defines all the buttons (3) and sets their onClick to the method below
+        Button writeIdBtn = (Button) findViewById(R.id.write_id);
+        Button actAsIdBtn = (Button) findViewById(R.id.act_as_id);
+        Button actAsLocBtn = (Button) findViewById(R.id.act_as_loc);
+        writeIdBtn.setOnClickListener(this);
+        actAsLocBtn.setOnClickListener(this);
+        actAsIdBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        //creates intent
+        Intent intent;
+
+        //runs a switch based on button ID and sets the intent accordingly
+        switch(v.getId()){
+            case R.id.write_id:
+                intent = new Intent(this, WriteActivity.class);
+                break;
+            case R.id.act_as_id:
+                intent = new Intent(this, SendActivity.class);
+                break;
+            case R.id.act_as_loc:
+                intent = new Intent(this, LocActivity.class);
+                break;
+            default:
+                intent = new Intent(this, MainActivity.class);
+                break;
+        }
+
+        //switches to according activity
+        startActivity(intent);
     }
 }
