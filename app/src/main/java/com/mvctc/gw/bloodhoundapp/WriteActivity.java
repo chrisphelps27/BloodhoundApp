127e8ddbae4bb7b199b2cfe8c1dbb70ef5e7be25
phelps47387 <chrisphelps2727@gmail.com>
2017-10-27 12:03:06 -0400

    Implements the buttons added to go back to main activity

--- app/src/main/java/com/mvctc/gw/bloodhoundapp/WriteActivity.java
+++ app/src/main/java/com/mvctc/gw/bloodhoundapp/WriteActivity.java
@@ -1,19 +1,42 @@
 package com.mvctc.gw.bloodhoundapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by phelps47387 on 10/26/2017.
  */
 
-public class WriteActivity {
-    public class SendActivity extends AppCompatActivity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            //TODO change this to the correct activity once created
-            setContentView(R.layout.activity_main);
+public class WriteActivity extends AppCompatActivity implements View.OnClickListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TODO change this to the correct activity once created
+        setContentView(R.layout.activity_write);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        //creates intent
+        Intent intent;
+
+        //runs a switch based on button ID and sets the intent accordingly
+        switch(v.getId()){
+            case R.id.button:
+                intent = new Intent(this, MainActivity.class);
+                break;
+            default:
+                intent = new Intent(this, MainActivity.class);
+                break;
         }
+
+        //switches to according activity
+        startActivity(intent);
     }
 }
